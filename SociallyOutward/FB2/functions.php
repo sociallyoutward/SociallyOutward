cc70332748536a85b202c03a1c85054443c86b5d
Arie <awprogramming@gmail.com>
2014-05-21 20:57:56 -0400

    Continue taking from backup

---
+++ SociallyOutward/FB2/functions.php
@@ -0,0 +1,17 @@
+<?php
+require 'dbconfig.php';
+function checkuser($fuid,$funame,$ffname,$femail){
+    		
+   //  		$sql="SELECT * FROM Users WHERE Fuid='" . $fuid . "'";
+   			
+			// $check = $connection->query($sql);
+			// $check = 
+			// if ($check->num_rows==0) { // if new user . Insert a new record		
+			// $query = "INSERT INTO Users (Fuid,Funame,Ffname,Femail) VALUES ('$fuid','$funame','$ffname','$femail')";
+			// $connection->query($query);	
+			// } else {   // If Returned user . update the user record		
+			// $query = "UPDATE Users SET Funame='$funame', Ffname='$ffname', Femail='$femail' where Fuid='$fuid'";
+			// $connection->query($query);	
+			// }
+
+}?>
